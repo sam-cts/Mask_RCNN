28196c966cb4969b7a6caedc0f5f81682f22e774
sam-cts <sam.choy2309@hotmail.com>
2019-04-02 06:20:41 +0800

    started finding contour to be finished

---
+++ ProjectDemo/human_parsing/finding_contour.py
@@ -0,0 +1,10 @@
+#%% A Test Demo to find contours
+import os
+import sys
+import cv2 as cv
+import numpy as np 
+import matplotlib.pyplot as plt 
+
+# Root directory of the project
+ROOT_DIR = os.path.abspath("../../")
+
